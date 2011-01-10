8419d92d62fd2e614525cc6959cf17c9f846ba9a
u.laufs@gmail.com <u.laufs@gmail.com@525f8095-7601-c394-9fa2-63fb1eb71d86>
2011-01-10 14:40:55 +0000

    Minor changes in Circular Menu example.

--- trunk/_Examples and Tests/extension examples/org/mt4jx/examples/circularmenu/MTCircularMenuExampleScene.java
+++ trunk/_Examples and Tests/extension examples/org/mt4jx/examples/circularmenu/MTCircularMenuExampleScene.java
@@ -15,25 +15,28 @@ import org.mt4j.sceneManagement.AbstractScene;
 import org.mt4j.util.MTColor;
 import org.mt4j.util.math.Vector3D;
 import org.mt4jx.components.visibleComponents.widgets.circularmenu.AnimationUtil;
+import org.mt4jx.components.visibleComponents.widgets.circularmenu.CircularMenuSegmentHandle;
 import org.mt4jx.components.visibleComponents.widgets.circularmenu.MTCircularMenu;
 
 public class MTCircularMenuExampleScene extends AbstractScene {
-
+	private MTApplication app;
 	public MTCircularMenuExampleScene(final MTApplication mtApplication, String name) {
 		super(mtApplication, name);
+		this.app = mtApplication;
 		
 		MTColor black = new MTColor(0,0,0);
-		this.setClearColor(new MTColor(188, 150, 146, 255));
+		MTColor white = new MTColor(255,255,255);
+		this.setClearColor(new MTColor(150, 150, 150, 255));
 		//Show touches
 		this.registerGlobalInputProcessor(new CursorTracer(mtApplication, this));
 		
-		IFont fontArial = FontManager.getInstance().createFont(mtApplication, "arial.ttf", 
-				50, 	//Font size
-				black);	//Font color
-		//Create a textfield
+		IFont fontArial = FontManager.getInstance().createFont(mtApplication, "arial.ttf", 50, white);	
+
+		
 		final MTTextArea textField = new MTTextArea(mtApplication, fontArial); 		
 		textField.setStrokeColor(black);
-		textField.setText("click me!");
+		textField.setFillColor(black);
+		textField.setText("touch me!");
 		//Center the textfield on the screen
 		textField.setPositionGlobal(new Vector3D(mtApplication.width/2f, mtApplication.height/2f));
 		//Add the textfield to our canvas
@@ -48,29 +51,30 @@ public class MTCircularMenuExampleScene extends AbstractScene {
 					switch (te.getId()) {
 					case TapEvent.GESTURE_ENDED:
 						if (te.isTapped()) {
-							int id;
-							final MTCircularMenu menu = new MTCircularMenu(mtApplication, 45);
-							id = menu.addItem("Cancel");
-							menu.setSegmentColor(id, new MTColor(0, 196, 0, 164));
-							id = menu.addItem("Remove");
-							menu.setSegmentColor(id, new MTColor(196, 0, 0, 164));
-							menu.addActionListener(id, new ActionListener() {
+							CircularMenuSegmentHandle segment;
+							final MTCircularMenu menu = new MTCircularMenu(mtApplication, 60);
+
+							segment = menu.createSegment("Cancel");
+							segment.setFillColor(new MTColor(0, 127, 0, 255-32));
+							segment = menu.createSegment("Remove");
+							segment.setFillColor(new MTColor(127, 0, 0, 255-32));
+							segment.addActionListener(new ActionListener() {
 								@Override
 								public void actionPerformed(ActionEvent e) {
 									AnimationUtil.bounceOut(textField, true);
 								}
 							});
-							id = menu.addItem("Rotate");
-							menu.setSegmentColor(id, new MTColor(0, 0, 196, 164));
-							menu.addActionListener(id, new ActionListener() {
+							segment = menu.createSegment("Rotate");
+							segment.addActionListener(new ActionListener() {
 								@Override
 								public void actionPerformed(ActionEvent e) {
-									AnimationUtil.rotate2D(textField, 120);
+									AnimationUtil.rotate2D(textField, 110);
 								}
 							});
 							menu.setPositionGlobal(textField.getCenterPointGlobal());
 							getCanvas().addChild(menu);
 							AnimationUtil.scaleIn(menu);
+							AnimationUtil.moveIntoScreen(menu, app);
 						}
 						break;
 					default:
