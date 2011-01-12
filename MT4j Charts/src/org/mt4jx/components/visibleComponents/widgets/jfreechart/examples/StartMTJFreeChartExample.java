dc014720d5127bd0530a956f5b73550484bf89a8
u.laufs <u.laufs@gmail.com>
2011-01-12 13:22:01 +0000

    moved examples to extension project.

---
+++ MT4j Charts/src/org/mt4jx/components/visibleComponents/widgets/jfreechart/examples/StartMTJFreeChartExample.java
@@ -0,0 +1,33 @@
+package org.mt4jx.components.visibleComponents.widgets.jfreechart.examples;
+
+import org.mt4j.MTApplication;
+
+/***********************************************************************
+ * MT4jCharts, created April 2010
+ * by Uwe Laufs
+ *  
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
+public class StartMTJFreeChartExample extends MTApplication {
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) {
+		initialize();
+	}
+	@Override
+	public void startUp() {
+		addScene(new MTJFreeChartExampleScene(this, "Chart Example Scene"));
+	}
+}
