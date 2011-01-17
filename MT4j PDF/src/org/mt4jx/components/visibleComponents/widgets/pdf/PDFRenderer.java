0cfdc3fc6e9bd3881b592e60cfb734f8b49bfd9f
u.laufs <u.laufs@gmail.com>
2011-01-17 10:14:44 +0000

    added comments.

--- MT4j PDF/src/org/mt4jx/components/visibleComponents/widgets/pdf/PDFRenderer.java
+++ MT4j PDF/src/org/mt4jx/components/visibleComponents/widgets/pdf/PDFRenderer.java
@@ -1,3 +1,20 @@
+/***********************************************************************
+ *   MT4j Extension: PDF
+ *   
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License (LGPL)
+ *   as published by the Free Software Foundation, either version 3
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the LGPL
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***********************************************************************/
 package org.mt4jx.components.visibleComponents.widgets.pdf;
 import java.awt.Image;
 import java.awt.Rectangle;
@@ -12,7 +29,10 @@ import processing.core.PImage;
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
-
+/**
+ * @author Uwe Laufs
+ *
+ */
 public class PDFRenderer {
 	
 	public static Image loadImage(File pdf, double scaleFactor, int pageNumber) throws IOException {
