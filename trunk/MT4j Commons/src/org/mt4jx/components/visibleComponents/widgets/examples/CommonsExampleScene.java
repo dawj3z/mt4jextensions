24f86c6a094b957de2978d2717dc36d7e8f70b85
u.laufs@gmail.com <u.laufs@gmail.com@525f8095-7601-c394-9fa2-63fb1eb71d86>
2011-02-09 15:45:28 +0000

    changed MTInfoPanel

--- trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/examples/CommonsExampleScene.java
+++ trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/examples/CommonsExampleScene.java
@@ -2,7 +2,6 @@ package org.mt4jx.components.visibleComponents.widgets.examples;
 import org.mt4j.MTApplication;
 import org.mt4j.components.visibleComponents.shapes.MTRectangle;
 import org.mt4j.sceneManagement.AbstractScene;
-import org.mt4jx.components.visibleComponents.shapes.widgets.imageinfo.Foo;
 import org.mt4jx.components.visibleComponents.shapes.widgets.imageinfo.MTInfoPanel;
 
 public class CommonsExampleScene extends AbstractScene {
@@ -12,10 +11,8 @@ public class CommonsExampleScene extends AbstractScene {
 		super(mtApplication, name);
 		this.app = mtApplication;
 		MTRectangle rect = new MTRectangle(mtApplication, mtApplication.loadImage("./data/logo.gif"));
-		MTInfoPanel ip = new MTInfoPanel(rect, mtApplication, "This is the label text.", "This is the text. This is the text. This is the text. This is the text. This is the text. This is the text.", 500, 300);
-		this.getCanvas().addChild(ip);
 		
-		Foo foo = new Foo(mtApplication);
+		MTInfoPanel foo = new MTInfoPanel(mtApplication, "LabelText LabelText LabelText LabelText LabelText", rect, "Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. Text. ", 500, 200);
 		this.getCanvas().addChild(foo);
 	}
 }
