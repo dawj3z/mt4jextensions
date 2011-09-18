d8b97b2ac54eaf694c4a0be3012f9dbe6d205dee
u.laufs@googlemail.com <u.laufs@googlemail.com@525f8095-7601-c394-9fa2-63fb1eb71d86>
2011-09-18 22:21:31 +0000

    several changes ...

--- trunk/MT4j PDF/src/org/mt4jx/components/visibleComponents/widgets/pdf/PDFRenderer.java
+++ trunk/MT4j PDF/src/org/mt4jx/components/visibleComponents/widgets/pdf/PDFRenderer.java
@@ -19,7 +19,6 @@ package org.mt4jx.components.visibleComponents.widgets.pdf;
 import java.awt.Image;
 import java.awt.Rectangle;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
@@ -27,7 +26,6 @@ import java.nio.channels.FileChannel;
 
 import processing.core.PImage;
 
-import com.sun.pdfview.Cache;
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
 
@@ -47,7 +45,18 @@ public class PDFRenderer {
         PDFPage page = pdffile.getPage(pageNumber);
 		int w = (int)(page.getBBox().getWidth());
 		int h = (int)(page.getBBox().getHeight());
-        Rectangle clip = new Rectangle(0,0,w,h);
+		System.out.println("dimension pdfFile: " + w + "/" + h);
+		
+		int c1,c2; // for clipping
+		if(w>h){
+			c1 = h;
+			c2 = w;
+		}else{
+			c1 = w;
+			c2 = h;
+		}
+        Rectangle clip = new Rectangle(0,0,(int)(c1*scaleFactor),(int) (c2*scaleFactor) );
+        
         Image img = page.getImage(
         		(int)(w*scaleFactor),(int)(h*scaleFactor), //width & height
                 clip, // clip rect
@@ -55,7 +64,9 @@ public class PDFRenderer {
                 true, // fill background with white
                 true  // block until drawing is done
                 );
-        RenderedPDFPage result = new RenderedPDFPage(pdf, new PImage(img), pageNumber, w*h);
+        RenderedPDFPage result = new RenderedPDFPage(pdf, new PImage(img), pageNumber, (int)(w*scaleFactor*h*scaleFactor) );
+        System.out.println("Rendered dimension:" + result.getPImage().width + "/" + result.getPImage().height);
+        System.gc(); 
         return result;
 	}
 	
@@ -71,7 +82,15 @@ public class PDFRenderer {
 		int w = (int)(page.getBBox().getWidth());
 		int h = (int)(page.getBBox().getHeight());
 		
-        Rectangle clip = new Rectangle(0,0,(int)page.getBBox().getWidth(),(int)page.getBBox().getHeight());
+		int c1,c2; // for clipping
+		if(w>h){
+			c1 = (int)page.getBBox().getHeight();
+			c2 = (int)page.getBBox().getWidth();
+		}else{
+			c1 = (int)page.getBBox().getWidth();
+			c2 = (int)page.getBBox().getHeight();
+		}
+        Rectangle clip = new Rectangle(0,0,c1,c2);
         Image img = page.getImage(
         		width, height, //width & height
                 clip, // clip rect
@@ -80,6 +99,7 @@ public class PDFRenderer {
                 true  // block until drawing is done
                 );
         RenderedPDFPage result = new RenderedPDFPage(pdf, new PImage(img), pageNumber, w*h);
+        System.gc(); 
         return result;
 	}
 	public static RenderedPDFPage loadImage(File pdf) throws IOException {
