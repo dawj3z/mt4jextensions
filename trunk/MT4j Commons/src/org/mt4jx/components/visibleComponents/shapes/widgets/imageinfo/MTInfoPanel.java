bdec8ce68ed910fede3c5b16475a780984d8b766
ryan.lamothe@gmail.com <ryan.lamothe@gmail.com@525f8095-7601-c394-9fa2-63fb1eb71d86>
2013-07-23 18:28:02 +0000

    Refactored System.out.println to use Logging framework. This allows proper control of logging/verbose output in real applications.

--- trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/shapes/widgets/imageinfo/MTInfoPanel.java
+++ trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/shapes/widgets/imageinfo/MTInfoPanel.java
@@ -8,114 +8,132 @@ import org.mt4j.components.visibleComponents.shapes.MTLine;
 import org.mt4j.components.visibleComponents.shapes.MTRoundRectangle;
 import org.mt4j.components.visibleComponents.widgets.MTTextArea;
 import org.mt4j.util.MTColor;
+import org.mt4j.util.logging.ILogger;
+import org.mt4j.util.logging.MTLoggerFactory;
 import org.mt4j.util.math.Vector3D;
 import org.mt4jx.components.visibleComponents.layout.MTColumnLayout2D;
 
 import processing.core.PApplet;
 
 public class MTInfoPanel extends MTRoundRectangle {
-	private MTColumnLayout2D rows;
-	
-	private IFont headlineFont,textFont;
-	private float strokeWeight = 2.5f;
-	private MTColor textColor = new MTColor(255,255,255);
-	private MTColor fillColor = new MTColor(0,0,0, 192);
-	private MTColor strokeColor = new MTColor(255,255,255, 128);
-	private float maxWidth, maxHeight;
-	private String text,labelText;
-	private PApplet pa;
-	private AbstractShape image;
-	
-	public MTInfoPanel(PApplet pa, String labelText, AbstractShape image, String text, float maxWidth, float maxHeight){
-		this(pa, labelText, image, text, maxWidth, maxHeight, null, null);
-	}
-	public MTInfoPanel(PApplet pa, String labelText, AbstractShape image, String text, float maxWidth, float maxHeight, IFont headlineFont, IFont textFont){
-		super(pa,0,0,0,400,300,12,12);
-		this.setFillColor(fillColor);
-		this.setStrokeColor(strokeColor);
-		this.maxWidth = maxWidth;
-		this.maxHeight = maxHeight;
-		this.headlineFont = headlineFont;
-		this.textFont = textFont;
-		this.text = text;
-		this.labelText = labelText;
-		this.pa = pa;
-		this.image = image;
-		this.init();
-	}
-	
-	private void init(){
-		this.rows = new MTColumnLayout2D(pa);
-		this.rows.setPickable(false);
-		this.addChild(rows);
-		this.rows.translate(new Vector3D(1,1));
-		
-		// setting default font if missing
-		if(this.headlineFont==null){
-			this.headlineFont =  FontManager.getInstance().createFont(pa, "arial", 
-					32, 	//Font size
-					textColor);	//Font outline color
-		}
-		if(this.textFont==null){
-			this.textFont =  FontManager.getInstance().createFont(pa, "arial", 
-					16, 	//Font size
-					textColor);	//Font outline color
-		}
-		MTTextArea ta_label = new MTTextArea(pa,0,0,maxWidth, 40,headlineFont);
-			ta_label.setNoFill(true);
-			ta_label.setNoStroke(true);
-			ta_label.setPickable(false);
-			if(labelText!=null){
-				ta_label.setText(labelText);
-			}
-			this.rows.addChild(ta_label);
-			MTLine line = new MTLine(pa, 0,0,maxWidth,0);
-			line.setStrokeColor(strokeColor);
-			line.setStrokeWeight(strokeWeight);
-			line.setPickable(false);
-			this.rows.addChild(line);
-			
-			image.setNoStroke(true);
-			image.setPickable(false);
-			this.rows.addChild(image);
-			
-			float textHeight = maxHeight-rows.getHeightXY(TransformSpace.GLOBAL);
-			if(textHeight<90){
-				textHeight=90;
-			}
-			
-			line = new MTLine(pa, 0,0,maxWidth,0);
-			line.setStrokeColor(strokeColor);
-			line.setStrokeWeight(strokeWeight);
-			line.setPickable(false);
-			this.rows.addChild(line);
-			
-			MTTextArea ta_text = new MTTextArea(pa,0,0,maxWidth, textHeight,textFont);
-			ta_text.setNoFill(true);
-			ta_text.setNoStroke(true);
-			ta_text.setPickable(false);
-			if(text!=null){
-				ta_text.setText(text);
-			}
-			this.rows.addChild(ta_text);
-			this.setSizeLocal(this.rows.getWidthXY(TransformSpace.LOCAL), this.rows.getHeightXY(TransformSpace.LOCAL));
-			{
-			float w = this.getWidthXY(TransformSpace.LOCAL);
-			float h = this.getHeightXY(TransformSpace.LOCAL);
-//			System.out.println("w/h" + w + "/" + h);
-			if(w>maxWidth || h>maxHeight){
-				float fw = maxWidth/w;
-				float fh = maxHeight/h;
-//				System.out.println("fw/fh" + fw + "/" + fh);
-				if(fw<fh){
-					scale(fw, fw, fw, this.getCenterPointLocal());
-				}else{
-					scale(fh, fh, fh, this.getCenterPointLocal());
-				}
-			}
-	}
-			float w = this.getWidthXY(TransformSpace.GLOBAL);
-			float h = this.getHeightXY(TransformSpace.GLOBAL);
-			System.out.println("w/h" + w + "/" + h);
-	}
+
+    private static final ILogger LOG = MTLoggerFactory.getLogger(MTInfoPanel.class.getName());
+
+    static {
+        LOG.setLevel(ILogger.DEBUG);
+    }
+
+    private MTColumnLayout2D rows;
+
+    private IFont headlineFont, textFont;
+
+    private final float strokeWeight = 2.5f;
+
+    private final MTColor textColor = new MTColor(255, 255, 255);
+
+    private final MTColor fillColor = new MTColor(0, 0, 0, 192);
+
+    private final MTColor strokeColor = new MTColor(255, 255, 255, 128);
+
+    private final float maxWidth, maxHeight;
+
+    private final String text, labelText;
+
+    private final PApplet pa;
+
+    private final AbstractShape image;
+
+    public MTInfoPanel(PApplet pa, String labelText, AbstractShape image, String text, float maxWidth, float maxHeight) {
+        this(pa, labelText, image, text, maxWidth, maxHeight, null, null);
+    }
+
+    public MTInfoPanel(PApplet pa, String labelText, AbstractShape image, String text, float maxWidth, float maxHeight, IFont headlineFont, IFont textFont) {
+        super(pa, 0, 0, 0, 400, 300, 12, 12);
+        setFillColor(fillColor);
+        setStrokeColor(strokeColor);
+        this.maxWidth = maxWidth;
+        this.maxHeight = maxHeight;
+        this.headlineFont = headlineFont;
+        this.textFont = textFont;
+        this.text = text;
+        this.labelText = labelText;
+        this.pa = pa;
+        this.image = image;
+        init();
+    }
+
+    private void init() {
+        rows = new MTColumnLayout2D(pa);
+        rows.setPickable(false);
+        this.addChild(rows);
+        rows.translate(new Vector3D(1, 1));
+
+        // setting default font if missing
+        if (headlineFont == null) {
+            headlineFont = FontManager.getInstance().createFont(pa, "arial",
+                    32, // Font size
+                    textColor); // Font outline color
+        }
+        if (textFont == null) {
+            textFont = FontManager.getInstance().createFont(pa, "arial",
+                    16, // Font size
+                    textColor); // Font outline color
+        }
+        final MTTextArea ta_label = new MTTextArea(pa, 0, 0, maxWidth, 40, headlineFont);
+        ta_label.setNoFill(true);
+        ta_label.setNoStroke(true);
+        ta_label.setPickable(false);
+        if (labelText != null) {
+            ta_label.setText(labelText);
+        }
+        rows.addChild(ta_label);
+        MTLine line = new MTLine(pa, 0, 0, maxWidth, 0);
+        line.setStrokeColor(strokeColor);
+        line.setStrokeWeight(strokeWeight);
+        line.setPickable(false);
+        rows.addChild(line);
+
+        image.setNoStroke(true);
+        image.setPickable(false);
+        rows.addChild(image);
+
+        float textHeight = maxHeight - rows.getHeightXY(TransformSpace.GLOBAL);
+        if (textHeight < 90) {
+            textHeight = 90;
+        }
+
+        line = new MTLine(pa, 0, 0, maxWidth, 0);
+        line.setStrokeColor(strokeColor);
+        line.setStrokeWeight(strokeWeight);
+        line.setPickable(false);
+        rows.addChild(line);
+
+        final MTTextArea ta_text = new MTTextArea(pa, 0, 0, maxWidth, textHeight, textFont);
+        ta_text.setNoFill(true);
+        ta_text.setNoStroke(true);
+        ta_text.setPickable(false);
+        if (text != null) {
+            ta_text.setText(text);
+        }
+        rows.addChild(ta_text);
+        setSizeLocal(rows.getWidthXY(TransformSpace.LOCAL), rows.getHeightXY(TransformSpace.LOCAL));
+        {
+            final float w = getWidthXY(TransformSpace.LOCAL);
+            final float h = getHeightXY(TransformSpace.LOCAL);
+            // System.out.println("w/h" + w + "/" + h);
+            if ((w > maxWidth) || (h > maxHeight)) {
+                final float fw = maxWidth / w;
+                final float fh = maxHeight / h;
+                // System.out.println("fw/fh" + fw + "/" + fh);
+                if (fw < fh) {
+                    scale(fw, fw, fw, getCenterPointLocal());
+                } else {
+                    scale(fh, fh, fh, getCenterPointLocal());
+                }
+            }
+        }
+        final float w = getWidthXY(TransformSpace.GLOBAL);
+        final float h = getHeightXY(TransformSpace.GLOBAL);
+        LOG.debug("w/h" + w + "/" + h);
+    }
 }
--- trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/tabbedPanel/MTToggleImageButtonGroup.java
+++ trunk/MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/tabbedPanel/MTToggleImageButtonGroup.java
@@ -2,41 +2,55 @@ package org.mt4jx.components.visibleComponents.widgets.tabbedPanel;
 
 import java.util.ArrayList;
 
+import org.mt4j.util.logging.ILogger;
+import org.mt4j.util.logging.MTLoggerFactory;
+
 public class MTToggleImageButtonGroup implements MTToggleImageButtonListener {
-	private ArrayList<MTToggleImageButton> toggleButtons = new ArrayList<MTToggleImageButton>();
-	public MTToggleImageButtonGroup(){
-	}
-	public void add(MTToggleImageButton tib){
-		if(!this.toggleButtons.contains(tib)){
-			this.toggleButtons.add(tib);
-			tib.addToggleListener(this);
-		}
-		if(this.toggleButtons.size()==1){
-			setSelectedButton(tib);
-		}else{
-			tib.setSelected(false);
-		}
-	}
-	public void remove(MTToggleImageButton tib){
-		if(this.toggleButtons.contains(tib)){
-			this.toggleButtons.remove(tib);
-			tib.removeToggleListener(this);
-		}
-	}
-	@Override
-	public void buttonClicked(MTToggleImageButton button) {
-		System.out.println("MTToggleImageButtonGroup.buttonClicked(MTToggleImageButton button)");
-		this.setSelectedButton(button);
-	}
-	private void setSelectedButton(MTToggleImageButton button){
-		for (int i = 0; i < this.toggleButtons.size(); i++) {
-			MTToggleImageButton current = this.toggleButtons.get(i);
-			System.out.println("DESELECT: " + current);
-			current.setSelected(false);
-		}
-		if(!button.isSelected()){
-			button.setSelected(true);
-		}
-	}
-	
+
+    private static final ILogger LOG = MTLoggerFactory.getLogger(MTToggleImageButtonGroup.class.getName());
+
+    static {
+        LOG.setLevel(ILogger.DEBUG);
+    }
+
+    private final ArrayList<MTToggleImageButton> toggleButtons = new ArrayList<MTToggleImageButton>();
+
+    public MTToggleImageButtonGroup() {
+    }
+
+    public void add(MTToggleImageButton tib) {
+        if (!toggleButtons.contains(tib)) {
+            toggleButtons.add(tib);
+            tib.addToggleListener(this);
+        }
+        if (toggleButtons.size() == 1) {
+            setSelectedButton(tib);
+        } else {
+            tib.setSelected(false);
+        }
+    }
+
+    public void remove(MTToggleImageButton tib) {
+        if (toggleButtons.contains(tib)) {
+            toggleButtons.remove(tib);
+            tib.removeToggleListener(this);
+        }
+    }
+
+    @Override
+    public void buttonClicked(MTToggleImageButton button) {
+        LOG.debug("MTToggleImageButtonGroup.buttonClicked(MTToggleImageButton button)");
+        setSelectedButton(button);
+    }
+
+    private void setSelectedButton(MTToggleImageButton button) {
+        for (int i = 0; i < toggleButtons.size(); i++) {
+            final MTToggleImageButton current = toggleButtons.get(i);
+            LOG.debug("DESELECT: " + current);
+            current.setSelected(false);
+        }
+        if (!button.isSelected()) {
+            button.setSelected(true);
+        }
+    }
 }
