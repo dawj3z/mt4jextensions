8078e8aae74b5a8a2befee964858a1530d9d35cf
ryan.lamothe@gmail.com <ryan.lamothe@gmail.com@525f8095-7601-c394-9fa2-63fb1eb71d86>
2013-07-23 17:35:58 +0000

    Refactored System.out.println to use Logging framework. This allows proper control of logging/verbose output in real applications.

--- trunk/MT4j Drag&Drop/src/org/mt4jx/input/gestureAction/dnd/AbstractDnDAction.java
+++ trunk/MT4j Drag&Drop/src/org/mt4jx/input/gestureAction/dnd/AbstractDnDAction.java
@@ -14,153 +14,175 @@ import org.mt4j.input.inputProcessors.IGestureEventListener;
 import org.mt4j.input.inputProcessors.MTGestureEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
 import org.mt4j.input.inputProcessors.componentProcessors.scaleProcessor.ScaleEvent;
+import org.mt4j.util.logging.ILogger;
+import org.mt4j.util.logging.MTLoggerFactory;
 import org.mt4j.util.math.Vector3D;
+
 /**
  * Office Layout Enginering, Project Semantic Touch, (C) 2009 Fraunhofer IAO, all rights reserved.
+ * 
  * @author Uwe Laufs
  */
 public abstract class AbstractDnDAction implements IGestureEventListener {
-	/**
-	 * Abstract class that provides some functionality required for gesture actions.
+
+    private static final ILogger LOG = MTLoggerFactory.getLogger(AbstractDnDAction.class.getName());;
+
+    static {
+        LOG.setLevel(ILogger.DEBUG);
+    }
+
+    /**
+     * Abstract class that provides some functionality required for gesture actions.
+     * 
      * @author Uwe Laufs
-	 */
-	@Override
-	public boolean processGestureEvent(MTGestureEvent g) {
-		if(g instanceof DragEvent){
-		switch (g.getId()) {
-			case DragEvent.GESTURE_STARTED:
-				this.gestureDetected(g);
-				break;
-			case DragEvent.GESTURE_UPDATED:
-				this.gestureUpdated(g);
-				break;
-			case DragEvent.GESTURE_ENDED:
-				this.gestureEnded(g);
-				break;
-		}
-		}
-		return false;
-	}
-	protected MTCanvas getParentCanvas(MTComponent as) {
-		MTComponent tmp = as.getRoot();
-		if(tmp instanceof MTCanvas){
-			return (MTCanvas)tmp;
-		}else{
-			MTComponent mtc = as;
-			while ((!(mtc == null)) && (!((mtc = mtc.getParent()) instanceof MTCanvas))) {
-			}
-			return (MTCanvas) mtc;
-		}
-	}
-	/**
-	 * @param g
-	 * @return returns involved cursor position or null, if none available
-	 */
-	protected Vector3D getCursorPosition(MTGestureEvent g) {
-		if(g instanceof DragEvent){
-			DragEvent dragEvent = (DragEvent)g;
-			InputCursor cursor = dragEvent.getDragCursor();
-			AbstractCursorInputEvt lastCursorEvent = cursor.getEvents().get(cursor.getEventCount()-1);
-			return new Vector3D(lastCursorEvent.getX(), lastCursorEvent.getY());
-		}else if(g instanceof ScaleEvent){
-			//TODO implement
-			return null;
-		}else{
-			return null;
-		}
-	}
-	
-	
-	protected MTComponent getVisibileParentComponent(MTGestureEvent g){
-		IMTComponent3D inputComponent = g.getTarget();
-		if((inputComponent instanceof MTComponent)){
-			return getVisibileParentComponent((MTComponent)inputComponent);
-		}else{
-			return null;
-		}
-	}
-	/**
-	 * @param an MTComponent
-	 * @return the highest level parent AbstractVisibleComponent in the component hierarchy
-	 */
-	protected MTComponent getVisibileParentComponent(MTComponent sourceComponent){
-		MTComponent candidate = null;
-		{
-			MTComponent tmp = sourceComponent;
-			while( !((tmp=tmp.getParent()) instanceof MTCanvas) ){
-				if(tmp instanceof AbstractVisibleComponent){
-					candidate = (MTComponent)tmp;
-				}
-			}
-		}
-		if(candidate==null){
-			return sourceComponent;
-		}else{
-			return candidate;
-		}
-	}
-		
-	/**
-	 * This method is called in case GESTURE_DETECTED.
-	 * @param g The current MTGestureEvent
-	 * @return boolean consumed
-	 */
-	public abstract boolean gestureDetected(MTGestureEvent g);
-	/**
-	 * This method is called in case GESTURE_UPDATED.
-	 * @param g The current MTGestureEvent
-	 * @return boolean consumed
-	 */
-	public abstract boolean gestureUpdated(MTGestureEvent g);
-	/**
-	 * This method is called in case GESTURE_ENDED.
-	 * @param g The current MTGestureEvent
-	 * @return boolean consumed
-	 */
-	public abstract boolean gestureEnded(MTGestureEvent g);
-	
-	/**
-	 * @param g
-	 * @return The MTDropTarget that is selected by the current gesture event or
-	 *         null if there is no such MTDropTarget.
-	 */
-	protected DropTarget detectDropTarget(MTGestureEvent g, boolean pickableObjectsOnly) {
-		DropTarget returnValue = null;
-		MTComponent sourceComponent = (MTComponent) g.getTarget();
-		Vector3D componentDropPositions = this.getCursorPosition(g);
-		{
-				MTCanvas parentCanvas = this.getParentCanvas(sourceComponent);
-				PickResult prCanvas = parentCanvas.pick(componentDropPositions
-						.getX(), componentDropPositions.getY(), pickableObjectsOnly);
-				List<PickEntry> pickList = prCanvas.getPickList();
-				
-				for(int i=0; i<pickList.size(); i++){
-//				for (int i = pickList.size() - 1; i >= 0; i--) {
-					
-					System.out.println("PickList#" + pickList.size());
-					PickEntry currentPickEntry = pickList.get(i);
-					MTComponent currentComponent = currentPickEntry.hitObj;
-					// get "parent" of composed components for listener invocation
-//					currentComponent = this
-//							.getVisibileParentComponent(currentComponent);
-					// skip the currently dragged source component
-					if (!(currentComponent.equals(sourceComponent))) {
-						if (currentComponent instanceof DropTarget) {
-							DropTarget target = (DropTarget) currentComponent;
-							// Only pick a target that will accept the 
-							// source component.
-							if (target.dndAccept(sourceComponent)) {
-								System.out.println("DROP TARGET DETECTED:" +
-										target);
-								return target;
-							}
-						} else {
-						 System.out.println("NOT A DROP TARGET:" + currentComponent);
-						}
-					}
-				}
-		}
-//		System.out.println("return null");
-		return null;
-	}
+     */
+    @Override
+    public boolean processGestureEvent(MTGestureEvent g) {
+        if (g instanceof DragEvent) {
+            switch (g.getId()) {
+                case DragEvent.GESTURE_STARTED:
+                    gestureDetected(g);
+                    break;
+                case DragEvent.GESTURE_UPDATED:
+                    gestureUpdated(g);
+                    break;
+                case DragEvent.GESTURE_ENDED:
+                    gestureEnded(g);
+                    break;
+            }
+        }
+        return false;
+    }
+
+    protected MTCanvas getParentCanvas(MTComponent as) {
+        final MTComponent tmp = as.getRoot();
+        if (tmp instanceof MTCanvas) {
+            return (MTCanvas) tmp;
+        } else {
+            MTComponent mtc = as;
+            while ((!(mtc == null)) && (!((mtc = mtc.getParent()) instanceof MTCanvas))) {
+            }
+            return (MTCanvas) mtc;
+        }
+    }
+
+    /**
+     * @param g
+     * @return returns involved cursor position or null, if none available
+     */
+    protected Vector3D getCursorPosition(MTGestureEvent g) {
+        if (g instanceof DragEvent) {
+            final DragEvent dragEvent = (DragEvent) g;
+            final InputCursor cursor = dragEvent.getDragCursor();
+            final AbstractCursorInputEvt lastCursorEvent = cursor.getEvents().get(cursor.getEventCount() - 1);
+            return new Vector3D(lastCursorEvent.getX(), lastCursorEvent.getY());
+        } else if (g instanceof ScaleEvent) {
+            // TODO implement
+            return null;
+        } else {
+            return null;
+        }
+    }
+
+    protected MTComponent getVisibileParentComponent(MTGestureEvent g) {
+        final IMTComponent3D inputComponent = g.getTarget();
+        if ((inputComponent instanceof MTComponent)) {
+            return getVisibileParentComponent((MTComponent) inputComponent);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param an
+     *            MTComponent
+     * @return the highest level parent AbstractVisibleComponent in the component hierarchy
+     */
+    protected MTComponent getVisibileParentComponent(MTComponent sourceComponent) {
+        MTComponent candidate = null;
+        {
+            MTComponent tmp = sourceComponent;
+            while (!((tmp = tmp.getParent()) instanceof MTCanvas)) {
+                if (tmp instanceof AbstractVisibleComponent) {
+                    candidate = tmp;
+                }
+            }
+        }
+        if (candidate == null) {
+            return sourceComponent;
+        } else {
+            return candidate;
+        }
+    }
+
+    /**
+     * This method is called in case GESTURE_DETECTED.
+     * 
+     * @param g
+     *            The current MTGestureEvent
+     * @return boolean consumed
+     */
+    public abstract boolean gestureDetected(MTGestureEvent g);
+
+    /**
+     * This method is called in case GESTURE_UPDATED.
+     * 
+     * @param g
+     *            The current MTGestureEvent
+     * @return boolean consumed
+     */
+    public abstract boolean gestureUpdated(MTGestureEvent g);
+
+    /**
+     * This method is called in case GESTURE_ENDED.
+     * 
+     * @param g
+     *            The current MTGestureEvent
+     * @return boolean consumed
+     */
+    public abstract boolean gestureEnded(MTGestureEvent g);
+
+    /**
+     * @param g
+     * @return The MTDropTarget that is selected by the current gesture event or
+     *         null if there is no such MTDropTarget.
+     */
+    protected DropTarget detectDropTarget(MTGestureEvent g, boolean pickableObjectsOnly) {
+        final DropTarget returnValue = null;
+        final MTComponent sourceComponent = (MTComponent) g.getTarget();
+        final Vector3D componentDropPositions = getCursorPosition(g);
+        {
+            final MTCanvas parentCanvas = getParentCanvas(sourceComponent);
+            final PickResult prCanvas = parentCanvas.pick(componentDropPositions
+                    .getX(), componentDropPositions.getY(), pickableObjectsOnly);
+            final List<PickEntry> pickList = prCanvas.getPickList();
+
+            for (int i = 0; i < pickList.size(); i++) {
+                // for (int i = pickList.size() - 1; i >= 0; i--) {
+
+                LOG.debug("PickList#" + pickList.size());
+                final PickEntry currentPickEntry = pickList.get(i);
+                final MTComponent currentComponent = currentPickEntry.hitObj;
+                // get "parent" of composed components for listener invocation
+                // currentComponent = this
+                // .getVisibileParentComponent(currentComponent);
+                // skip the currently dragged source component
+                if (!(currentComponent.equals(sourceComponent))) {
+                    if (currentComponent instanceof DropTarget) {
+                        final DropTarget target = (DropTarget) currentComponent;
+                        // Only pick a target that will accept the
+                        // source component.
+                        if (target.dndAccept(sourceComponent)) {
+                            LOG.debug("DROP TARGET DETECTED:" + target);
+                            return target;
+                        }
+                    } else {
+                        LOG.debug("NOT A DROP TARGET:" + currentComponent);
+                    }
+                }
+            }
+        }
+        // System.out.println("return null");
+        return null;
+    }
 }
--- trunk/MT4j Drag&Drop/src/org/mt4jx/input/gestureAction/dnd/example/MyDragAndDropTarget.java
+++ trunk/MT4j Drag&Drop/src/org/mt4jx/input/gestureAction/dnd/example/MyDragAndDropTarget.java
@@ -9,68 +9,80 @@ import org.mt4j.components.visibleComponents.shapes.MTRectangle;
 import org.mt4j.components.visibleComponents.widgets.MTTextArea;
 import org.mt4j.input.inputProcessors.componentProcessors.dragProcessor.DragEvent;
 import org.mt4j.util.MTColor;
+import org.mt4j.util.logging.ILogger;
+import org.mt4j.util.logging.MTLoggerFactory;
 import org.mt4jx.input.gestureAction.dnd.DragAndDropTarget;
 
 import processing.core.PApplet;
 
 public class MyDragAndDropTarget extends MTRectangle implements DragAndDropTarget {
-	
-	private ArrayList<MTComponent> droppedComponents = new ArrayList<MTComponent>();
-	private MTTextArea text;
-	
-	public MyDragAndDropTarget(PApplet pApplet, IFont font) {
-		super(100, 100, 400, 200, pApplet);
-		
-		IFont arial = FontManager.getInstance().createFont(pApplet, "arial.ttf", 
-				30, 				  //Font size
-				new MTColor(0, 0, 0, 255),   //Font fill color
-				new MTColor(0, 0, 0, 255));
-		
-		this.text = new MTTextArea(pApplet, arial);
-		this.text.setNoFill(true);
-		this.text.setNoStroke(true);
-		this.setStrokeWeight(3f);
-		this.addChild(text);
-		this.text.setPositionGlobal(this.getCenterPointGlobal());
-		this.text.removeAllGestureEventListeners();
-		this.setPickable(false);
-	}
-	
-	public void setText(){
-		String text = this.getName() + "\n";
-		for (int i = 0; i < this.droppedComponents.size(); i++) {
-			text+= "- " + droppedComponents.get(i).getName() + "\n";
-		}
-		this.text.setText(text);
-		this.text.setPositionGlobal(this.getCenterPointGlobal());
-	}
 
-	@Override
-	public void componentDropped(MTComponent droppedComponent, DragEvent de) {
-		if(!droppedComponents.contains(droppedComponent)){
-			this.droppedComponents.add(droppedComponent);
-		}
+    /** The Constant logger. */
+    private static final ILogger LOG = MTLoggerFactory.getLogger(MyDragAndDropTarget.class.getName());
 
-		this.setStrokeColor(this.getFillColor());
-		System.out.println(this.getName() +": "+ droppedComponent.getName() + " dropped.");
-		this.setText();
-	}
-	@Override
-	public void componentEntered(MTComponent droppedComponent) {
-		System.out.println(this.getName() +": "+ droppedComponent.getName() + " entered.");
-		this.setStrokeColor(new MTColor(255,0,0));
-		this.setText();
-	}
-	@Override
-	public void componentExited(MTComponent droppedComponent) {
-		this.droppedComponents.remove(droppedComponent);
-		this.setStrokeColor(this.getFillColor());
-		System.out.println(this.getName() +": "+ droppedComponent.getName() + " exited.");
-		this.setText();
-	}
+    static {
+        LOG.setLevel(ILogger.DEBUG);
+    }
 
-	@Override
-	public boolean dndAccept(MTComponent component) {
-		return true;
-	}
+    private final ArrayList<MTComponent> droppedComponents = new ArrayList<MTComponent>();
+
+    private final MTTextArea text;
+
+    public MyDragAndDropTarget(PApplet pApplet, IFont font) {
+        super(100, 100, 400, 200, pApplet);
+
+        final IFont arial = FontManager.getInstance().createFont(pApplet, "arial.ttf",
+                30, // Font size
+                new MTColor(0, 0, 0, 255), // Font fill color
+                new MTColor(0, 0, 0, 255));
+
+        text = new MTTextArea(pApplet, arial);
+        text.setNoFill(true);
+        text.setNoStroke(true);
+        setStrokeWeight(3f);
+        this.addChild(text);
+        text.setPositionGlobal(getCenterPointGlobal());
+        text.removeAllGestureEventListeners();
+        setPickable(false);
+    }
+
+    public void setText() {
+        String text = getName() + "\n";
+        for (int i = 0; i < droppedComponents.size(); i++) {
+            text += "- " + droppedComponents.get(i).getName() + "\n";
+        }
+        this.text.setText(text);
+        this.text.setPositionGlobal(getCenterPointGlobal());
+    }
+
+    @Override
+    public void componentDropped(MTComponent droppedComponent, DragEvent de) {
+        if (!droppedComponents.contains(droppedComponent)) {
+            droppedComponents.add(droppedComponent);
+        }
+
+        setStrokeColor(getFillColor());
+        LOG.debug(getName() + ": " + droppedComponent.getName() + " dropped.");
+        setText();
+    }
+
+    @Override
+    public void componentEntered(MTComponent droppedComponent) {
+        LOG.debug(getName() + ": " + droppedComponent.getName() + " entered.");
+        setStrokeColor(new MTColor(255, 0, 0));
+        setText();
+    }
+
+    @Override
+    public void componentExited(MTComponent droppedComponent) {
+        droppedComponents.remove(droppedComponent);
+        setStrokeColor(getFillColor());
+        LOG.debug(getName() + ": " + droppedComponent.getName() + " exited.");
+        setText();
+    }
+
+    @Override
+    public boolean dndAccept(MTComponent component) {
+        return true;
+    }
 }
