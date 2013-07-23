f551f62f786c62043554fdd09788e9befb6aa8d7
ryan.lamothe <ryan.lamothe@gmail.com>
2013-07-23 18:28:02 +0000

    Refactored System.out.println to use Logging framework. This allows proper control of logging/verbose output in real applications.

--- MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/tabbedPanel/MTToggleImageButton.java
+++ MT4j Commons/src/org/mt4jx/components/visibleComponents/widgets/tabbedPanel/MTToggleImageButton.java
@@ -6,71 +6,89 @@ import java.util.ArrayList;
 
 import org.mt4j.components.visibleComponents.widgets.buttons.MTImageButton;
 import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapEvent;
-import org.mt4j.input.inputProcessors.componentProcessors.tapProcessor.TapProcessor;
 import org.mt4j.util.MTColor;
+import org.mt4j.util.logging.ILogger;
+import org.mt4j.util.logging.MTLoggerFactory;
 
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class MTToggleImageButton extends MTImageButton {
-	private MTImageButton overlayButton;
-	private boolean selected = false;
-	private ArrayList<MTToggleImageButtonListener> toggleListeners = new ArrayList<MTToggleImageButtonListener>();
-	private MTToggleImageButton selfRef;
-	public MTToggleImageButton(PImage texture, PApplet pApplet){
-		super(texture, pApplet);
-		this.setName("Unnamed Toggle Button");
-		this.selfRef = this;
-		this.overlayButton = new MTImageButton(texture, pApplet);
-		this.addChild(overlayButton);
-		this.overlayButton.setFillColor(new MTColor(0,0,0,128));
-		ActionListener al = new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				switch (e.getID()) {
-				case TapEvent.BUTTON_CLICKED:
-					for (int i = 0; i < toggleListeners.size(); i++) {
-						toggleListeners.get(i).buttonClicked(selfRef);
-					}
-					break;
 
-				default:
-					break;
-				}
-			}
-		};
-		this.overlayButton.addActionListener(al);
-		this.addActionListener(al);
-		this.setNoStroke(true);
-		this.overlayButton.setNoStroke(true);
-		this.toggle();
-	}
-	public void toggle(){
-		System.out.println("TOGGLE");
-		if(selected){
-			this.overlayButton.setVisible(true);
-			this.selected = false;
-		}else{
-			this.overlayButton.setVisible(false);
-			this.selected = true;
-		}
-	}
-	public void setSelected(boolean selected){
-		if(this.selected!=selected){
-			this.toggle();
-		}
-	}
-	public void addToggleListener(MTToggleImageButtonListener listener){
-		if(!this.toggleListeners.contains(listener)){
-			this.toggleListeners.add(listener);
-		}
-	}
-	public void removeToggleListener(MTToggleImageButtonListener listener){
-		this.toggleListeners.remove(listener);
-	}
-	@Override
-	public synchronized void addActionListener(ActionListener listener) {
-		super.addActionListener(listener);
-		this.overlayButton.addActionListener(listener);
-	}
+    private static final ILogger LOG = MTLoggerFactory.getLogger(MTToggleImageButton.class.getName());
+
+    static {
+        LOG.setLevel(ILogger.DEBUG);
+    }
+
+    private final MTImageButton overlayButton;
+
+    private boolean selected = false;
+
+    private final ArrayList<MTToggleImageButtonListener> toggleListeners = new ArrayList<MTToggleImageButtonListener>();
+
+    private final MTToggleImageButton selfRef;
+
+    public MTToggleImageButton(PImage texture, PApplet pApplet) {
+        super(texture, pApplet);
+        setName("Unnamed Toggle Button");
+        selfRef = this;
+        overlayButton = new MTImageButton(texture, pApplet);
+        this.addChild(overlayButton);
+        overlayButton.setFillColor(new MTColor(0, 0, 0, 128));
+        final ActionListener al = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch (e.getID()) {
+                    case TapEvent.BUTTON_CLICKED:
+                        for (int i = 0; i < toggleListeners.size(); i++) {
+                            toggleListeners.get(i).buttonClicked(selfRef);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        };
+        overlayButton.addActionListener(al);
+        addActionListener(al);
+        setNoStroke(true);
+        overlayButton.setNoStroke(true);
+        toggle();
+    }
+
+    public void toggle() {
+        LOG.debug("TOGGLE");
+        if (selected) {
+            overlayButton.setVisible(true);
+            selected = false;
+        } else {
+            overlayButton.setVisible(false);
+            selected = true;
+        }
+    }
+
+    @Override
+    public void setSelected(boolean selected) {
+        if (this.selected != selected) {
+            toggle();
+        }
+    }
+
+    public void addToggleListener(MTToggleImageButtonListener listener) {
+        if (!toggleListeners.contains(listener)) {
+            toggleListeners.add(listener);
+        }
+    }
+
+    public void removeToggleListener(MTToggleImageButtonListener listener) {
+        toggleListeners.remove(listener);
+    }
+
+    @Override
+    public synchronized void addActionListener(ActionListener listener) {
+        super.addActionListener(listener);
+        overlayButton.addActionListener(listener);
+    }
 }
